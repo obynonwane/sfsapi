049ffb84e6f5fdb1592b788808a5f912bb01444b
obinna <biostechnologyng@gmail.com>
2023-07-03 17:12:51 +0100

    Update

--- shell.sh
+++ shell.sh
@@ -3,11 +3,16 @@
 # Change to your Laravel project directory
 # cd /path/to/your/laravel/project
 
-# Run the passport:keys command
-php artisan passport:keys
+while true; do
+  # Run the passport:keys command
+  php artisan passport:keys
 
-# Set permissions for the private key
-chmod 600 storage/oauth-private.key
+  # Set permissions for the private key
+  chmod 600 storage/oauth-private.key
 
-# Set permissions for the public key
-chmod 600 storage/oauth-public.key
+  # Set permissions for the public key
+  chmod 600 storage/oauth-public.key
+
+  # Delay for 1 minute
+  sleep 60
+done
